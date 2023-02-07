066ecb1ca8666c298f75ea29be3c98df899650c6
Maksim Fililmonenkau <maxfilimonenkov@yahoo.com>
2023-02-07 10:20:09 +0300

    First init

---
+++ bot-mw/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
+)
+
+var botKeyboard = tu.Keyboard(
+	tu.KeyboardRow(
+		tu.KeyboardButton("/hi"),
+		tu.KeyboardButton("/help"),
+		tu.KeyboardButton("/status"),
+	),
+).WithResizeKeyboard()
+
+func main() {
+
+	bot, err := telego.NewBot(os.Getenv("TGBOT_API"), telego.WithDefaultDebugLogger())
+	if err != nil {
+		log.Panic(err)
+		os.Exit(1)
+	}
+
+	botUser, err := bot.GetMe()
+	if err != nil {
+		log.Panic(err)
+		os.Exit(1)
+	}
+
+	log.Printf("Work with account %v\n", botUser)
+
+	updates, err := bot.UpdatesViaLongPolling(nil)
+
+	bh, err := th.NewBotHandler(bot, updates)
+	if err != nil {
+		log.Panic(err)
+		os.Exit(1)
+	}
+
+	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+
+		var messageToUser string
+
+		switch message.Text {
+		case "/hi":
+			messageToUser = fmt.Sprintf("Привет, %s! Я твой бот-компаньон :)", message.From.FirstName)
+		case "/help":
+			messageToUser = fmt.Sprintf("Я понимаю команды /hi и /status) А смысл этого?....")
+		case "/status":
+			messageToUser = fmt.Sprintf("Я в порядке. Работаю... А ты?)")
+		}
+
+		_, _ = bot.SendMessage(tu.Messagef(
+			tu.ID(message.Chat.ID),
+			messageToUser,
+		))
+
+	}, th.AnyCommand())
+
+	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
+		chatID := tu.ID(message.Chat.ID)
+		_, _ = bot.CopyMessage(
+			tu.CopyMessage(chatID, chatID, message.MessageID).WithReplyMarkup(botKeyboard),
+		)
+	})
+
+	defer bh.Stop()
+	defer bot.StopLongPolling()
+
+	bh.Start()
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module bot-mw
+
+go 1.19
+
+require github.com/mymmrac/telego v0.20.1
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/fasthttp/router v1.4.16 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/savsgio/gotils v0.0.0-20230203094617-bcbc01813b4f // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.44.0 // indirect
+)
